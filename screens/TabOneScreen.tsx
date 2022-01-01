d00ef7c1d446454d77ebab68f1dead8fc3dad788
Ahmed <ahmed.abdelrahman3@vodafone.com>
2022-01-01 05:07:34 +0200

    slider && Social

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -5,8 +5,11 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import Avatar from '../src/Areas/Common/components/avatar/Avatar'; 
+import SocialIcon from '../src/Areas/Common/components/social/SocialIcon'
+import {margin} from '../src/Areas/Common/components/config/spacing';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
@@ -14,9 +17,15 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
         source={require('../assets/images/favicon.png')}
         size={60}
         rounded
-      >
-
+      >   
       </Avatar>
+      <SocialIcon
+                  light
+                  raised={false}
+                  type={'facebook'}
+                  style={styles.socialIconStyle}
+                  iconSize={15}
+                />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
@@ -37,5 +46,14 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  socialIconStyle: {
+    width: 32,
+    height: 32,
+    margin: 0,
+    marginHorizontal: margin.small / 2,
+    paddingTop: 0,
+    paddingBottom: 0,
+    marginBottom: margin.small,
   },
 });
